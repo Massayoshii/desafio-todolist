a07324406c5ae502e037490597eceab2982aa58f
Massayoshii <Massayoshibando@gmail.com>
2026-04-21 10:04:12 -0300

    update criado

--- src/main/java/br/com/desafio_todolist/service/TodoService.java
+++ src/main/java/br/com/desafio_todolist/service/TodoService.java
@@ -17,11 +17,17 @@ public class TodoService {
 
     public List <Todo> create(Todo todo){
         todoRepository.save(todo);
+        return list();
     }
 
     public List <Todo> list(){
         Sort sort = Sort.by("prioridade").descending().and
                 (Sort.by("nome")).ascending();
         return todoRepository.findAll(sort);
+    }
+
+    public List <Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
     }
 }
